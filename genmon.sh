374fc4adfc2c7bc91c5cdcede6934bac575a92da
Sergei A <varnie29a@mail.ru>
2024-01-04 00:02:09 +0500

    handle status

--- genmon.sh
+++ genmon.sh
@@ -11,17 +11,22 @@ check_internet() {
 if check_internet; then
 	# Fetch the country information using ipinfo.io
 	response=$(curl -s https://ipinfo.io)
+	status=$(echo "$response" | grep -o '"status": [0-9]*' | awk '{print $2}')
 	country=$(echo "$response" | grep -o '"country": "[^"]*' | cut -d'"' -f4)
 
-	# Set the color based on the country
-	if [ "$country" = "RU" ]; then
-		color="#FF0000"  # Red for Russia
+	if [ "$status" = "429" ]; then
+		echo "<txt><span weight=\"bold\" fgcolor=\"#FF0000\">not avail.</span></txt>"
 	else
-		color="#00FF00"  # Green for other countries
-	fi
+		# Set the color based on the country
+		if [ "$country" = "RU" ]; then
+			color="#FF0000"  # Red for Russia
+		else
+			color="#00FF00"  # Green for other countries
+		fi
 
-	# Display the country name in the specified color and bold style
-	echo "<txt><span weight=\"bold\" fgcolor=\"$color\">$country</span></txt>"
+		# Display the country name in the specified color and bold style
+		echo "<txt><span weight=\"bold\" fgcolor=\"$color\">$country</span></txt>"
+	fi
 
 	# Provide an empty tooltip (you can customize this with more information if needed)
 	echo "<tool></tool>"
